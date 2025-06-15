c1973ff8c2535cbad5c9ba40db77939d545449d2
SakinahInaa <sakinahismail15@gmail.com>
2025-06-15 21:37:06 +0700

    Update Jadwal Dosen.cpp

--- Jadwal Dosen.cpp
+++ Jadwal Dosen.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+struct MataKuliah {
+    string nama;
+    int sks;
+    string jenis;
+    vector<string> dosenPengampu;
+    string hari, jam, kelas, ruang;
+};
 
