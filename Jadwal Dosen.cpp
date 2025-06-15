01169b7d0f538b5b6c43a83e8e8968ea162bef90
fenyadria <fenyadriam@gmail.com>
2025-06-15 22:29:40 +0700

    Update Jadwal Dosen.cpp

--- Jadwal Dosen.cpp
+++ Jadwal Dosen.cpp
@@ -26,3 +26,28 @@ int getPrioritas(const string& jenis) {
 bool comparePrioritas(const MataKuliah& a, const MataKuliah& b) {
     return getPrioritas(a.jenis) > getPrioritas(b.jenis);
 }
+
+// Merge Sort untuk prioritas matkul
+void merge(vector<MataKuliah>& matkul, int left, int mid, int right) {
+    vector<MataKuliah> L(matkul.begin() + left, matkul.begin() + mid + 1);
+    vector<MataKuliah> R(matkul.begin() + mid + 1, matkul.begin() + right + 1);
+    int i = 0, j = 0, k = left;
+    while (i < L.size() && j < R.size()) {
+        if (getPrioritas(L[i].jenis) >= getPrioritas(R[j].jenis)) {
+            matkul[k++] = L[i++];
+        } else {
+            matkul[k++] = R[j++];
+        }
+    }
+    while (i < L.size()) matkul[k++] = L[i++];
+    while (j < R.size()) matkul[k++] = R[j++];
+}
+
+void mergeSort(vector<MataKuliah>& matkul, int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(matkul, left, mid);
+        mergeSort(matkul, mid + 1, right);
+        merge(matkul, left, mid, right);
+    }
+}
